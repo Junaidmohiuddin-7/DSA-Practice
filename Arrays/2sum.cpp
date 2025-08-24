81dfd408f623391e4b8ff8fc77f87b2af651c001
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-08-25 00:57:01 +0530

    Added optimized solution of 2 sum

---
+++ Arrays/2sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>maps;
+        for(int i=0;i<nums.size();i++){
+            int need=target-nums[i];
+            if(maps.find(need)!=maps.end()){
+return {maps[need],i};
+            }
+            maps[nums[i]]=i;
+        }
+        return {};
+    }
+};
