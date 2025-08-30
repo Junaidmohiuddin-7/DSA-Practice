fbddb8ddfba8fb9ca569fb090e883af43333480d
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-08-30 23:21:49 +0530

    all arrays solution

--- Arrays/missing-number.cpp
+++ Arrays/missing-number.cpp
@@ -34,3 +34,32 @@ ans=ans^nums[i];
 };
 Time Complexity: O(n)
 Space Complexity: O(1)
+//Another O(n)TC method which is a swapping method
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        while(i<n){
+            int correctindex=nums[i];
+            if(correctindex<n&&nums[i]!=nums[correctindex]){
+                swap(nums[i],nums[correctindex]);
+            }
+            else{
+                i++;
+            }
+
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i){
+                return i;
+            }
+
+        }
+        return n;
+    }
+};
+//what we are doing here is we are just swapping the element of array to its matching  index(matching the index and the element)
+
+
+
---
+++ Arrays/recursion basics/basics.cpp
@@ -0,0 +1,132 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <limits.h>
+using namespace std;
+// void printarray(int arr[],int size,int index){
+//     if(index==size){
+//         return ;
+//     }
+//     cout<<arr[index];
+//     printarray(arr,size,index+1);
+    
+    
+    
+    
+// }
+
+
+// int main()
+// {
+//   int arr[]={1,2,3,4,5,6};
+//   int size=6;
+//   int index=0;
+//   printarray(arr,size,index);
+   
+
+//     return 0;
+// }
+// bool search(int arr[],int size,int index,int target){
+//     if(index==size){
+//         return 0;
+//     }
+//     if(arr[index]==target){
+//       return 1;
+//     }
+//  bool ans= search(arr,size,index+1,target);
+//   return ans;
+    
+    
+  
+    
+// }
+
+// int main(){
+//     int arr[]={0,1,22,3,4,5};
+//     int size=6;
+//     int target=222;
+//     int index=0;
+//     bool ans=search(arr,size,index,target);
+//     cout<<ans;
+   
+// }
+
+// int maximum(int arr[],int size,int index,int &maxi){
+    
+    
+//     if(index==size){
+//         return maxi;
+//     }
+//     if(arr[index]>maxi){
+//         maxi=arr[index];
+    
+        
+//     }
+//     int ans=maximum(arr,size,index+1,maxi);
+//     return ans;
+    
+// }
+
+// int main(){
+//     int arr[]={1,22,3,44,5,6};
+//     int size=6;
+//     int index=0;
+//     int maxi=INT_MIN;
+//     maximum(arr,size,index,maxi);
+//     cout<<maxi;
+    
+// }
+// int maximum(int arr[],int size,int index,int &mini){
+    
+    
+//     if(index==size){
+//         return 0;
+//     }
+//     if(arr[index]<mini){
+//         mini=arr[index];
+//     }
+//     int ans=maximum(arr,size,index+1,mini);
+//     return ans;
+    
+// }
+
+// int main(){
+//     int arr[]={11,22,3,44,6,2};
+//     int size=6;
+//     int index=0;
+//     int mini=INT_MAX;
+//     maximum(arr,size,index,mini);
+//     cout<<mini;
+    
+// }
+// void printodd(int arr[],int size,int index){
+    
+//     if(arr[index]==size){
+//         return ;
+        
+//     }
+//     if((arr[index]&1)==1){
+//         cout<<arr[index];
+//     }
+//     return printodd(arr,size,index+1);
+    
+    
+// }
+
+
+
+
+// int main(){
+//     int arr[]={1,2,3,4,5,6,7,8,9};
+//     int size=0;
+//     int index=0;
+//     printodd(arr,size,index);
+    
+    
+// }
