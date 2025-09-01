9e69f2146fa08bfcfe25e4888291544106876f6b
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-09-01 23:47:22 +0530

    valid pal

---
+++ Strings/3.valid-palindrome2.cpp
@@ -0,0 +1,58 @@
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+// Example 1:
+
+// Input: s = "aba"
+// Output: true
+// Example 2:
+
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+// Example 3:
+
+// Input: s = "abc"
+// Output: false
+
+
+class Solution {
+public:
+
+bool check(string s,int i,int j){
+    while(i<=j){
+if(s[i]==s[j]){
+    i++;
+    j--;  
+}
+else{
+    return false;
+}
+    }
+return true;
+    
+}
+
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+if(s[i]==s[j]){
+    i++;
+    j--;
+}
+    else{
+bool ans1=check(s,i+1,j);
+bool ans2=check(s,i,j-1);
+bool ans=ans1||ans2;
+return ans;
+
+    }
+        }
+    
+return true;
+        
+    
+    }
+};
