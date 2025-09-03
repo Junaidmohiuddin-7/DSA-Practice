23beb065c6b0bbf05de191a1b8931fc208db4bc6
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-09-03 19:15:00 +0530

    sqrt using BS

---
+++ Arrays/Binary-search/6.sqrt(x).cpp
@@ -0,0 +1,50 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int s=0;
+        int e=x;
+        int ans=-1;
+        long long mid=s+(e-s)/2;
+        while(s<=e){
+            long long prod=(mid*mid);
+      if(prod==x){
+        return mid;
+      }
+      else if(prod>x){
+        
+e=mid-1;
+      }
+      else{
+        ans=mid;
+       s= mid+1;
+      }
+
+mid=s+(e-s)/2;
+
+
+
+        }
+        return ans;
+        
+    }
+};
