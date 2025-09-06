6457a6803b83fb4a5c83df2908f726ae2633c31e
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-09-06 23:52:59 +0530

    dividing without the use of / operators

---
+++ Arrays/Binary-search/7.divide-integer.cpp
@@ -0,0 +1,61 @@
+
+// Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+// The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+// Return the quotient after dividing dividend by divisor.
+
+// Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+ 
+
+// Example 1:
+
+// Input: dividend = 10, divisor = 3
+// Output: 3
+// Explanation: 10/3 = 3.33333.. which is truncated to 3.
+// Example 2:
+
+// Input: dividend = 7, divisor = -3
+// Output: -2
+// Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+
+
+
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+       
+        int anspos=true;
+        if(dividend<0&&divisor>0){
+            anspos=false;
+        }
+        if(dividend>0&&divisor<0){
+            anspos=false;
+        }
+
+        long long int dividendx = abs((long long)dividend);
+        long long int divisorx = abs((long long)divisor);
+        long long  int ans=0;
+        long long int s=0;
+        long long int e=dividendx;
+        while(s<=e){
+long long int mid=s+(e-s)/2;
+if(divisorx*mid<=dividendx){
+    ans=mid;
+    s=mid+1;
+}
+else{
+    e=mid-1;
+}
+
+
+        }
+        if(anspos==false){
+return -ans;
+        }
+return ans;
+        
+    }
+};
