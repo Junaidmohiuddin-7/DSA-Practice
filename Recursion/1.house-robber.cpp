10e258421b26ee13a3330acdaa7d841e0fefb5cf
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-09-19 23:08:53 +0530

    house robber with recursion

--- Recursion/1.house-robber.cpp
+++ Recursion/1.house-robber.cpp
@@ -13,3 +13,27 @@ public:
         return robber(nums, index);
     }
 };
+
+
+
+
+class Solution {
+public:
+    int calc(vector<int>& nums, int i) {
+
+        if (i >= nums.size()) {
+            return 0;
+        }
+
+        int rob = nums[i] + calc(nums, i + 2);
+        int skip = 0 + calc(nums, i + 1);
+        int anss = max(rob, skip);
+        return anss;
+    }
+
+    int rob(vector<int>& nums) {
+        int i = 0;
+        int ans = calc(nums, i);
+        return ans;
+    }
+};
