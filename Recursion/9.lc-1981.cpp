f290c84345045f42826fa041ed0d71d820496f03
junaid-dev-7 <junaidmohiuddin7hyd@gmail.com>
2025-10-05 23:45:43 +0530

    minimize the difference between target anf chosen elements

---
+++ Recursion/9.lc-1981.cpp
@@ -0,0 +1,64 @@
+You are given an m x n integer matrix mat and an integer target.
+
+Choose one integer from each row in the matrix such that the absolute difference between target and the sum of the chosen elements is minimized.
+
+// Return the minimum absolute difference.
+
+// The absolute difference between two numbers a and b is the absolute value of a - b.
+
+ 
+
+// Example 1:
+
+
+// Input: mat = [[1,2,3],[4,5,6],[7,8,9]], target = 13
+// Output: 0
+// Explanation: One possible choice is to:
+// - Choose 1 from the first row.
+// - Choose 5 from the second row.
+// - Choose 7 from the third row.
+// The sum of the chosen elements is 13, which equals the target, so the absolute difference is 0.
+// Example 2:
+
+
+// Input: mat = [[1],[2],[3]], target = 100
+// Output: 94
+// Explanation: The best possible choice is to:
+// - Choose 1 from the first row.
+// - Choose 2 from the second row.
+// - Choose 3 from the third row.
+// The sum of the chosen elements is 6, and the absolute difference is 94.
+// Example 3:
+
+
+// Input: mat = [[1,2,9,8,7]], target = 6
+// Output: 1
+// Explanation: The best choice is to choose 7 from the first row.
+// The absolute difference is 1.
+
+
+
+
+
+
+class Solution {
+public:
+    int check(vector<vector<int>>& mat, int target, int row, int sum,
+              int& mini) {
+        if (row == mat.size()) {
+            return abs(target - sum);
+        }
+
+        for (int c = 0; c < mat[0].size(); c++) {
+
+            mini =
+                min(mini, check(mat, target, row + 1, sum + mat[row][c], mini));
+        }
+        return mini;
+    }
+
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        int mini = INT_MAX;
+        return check(mat, target, 0, 0, mini);
+    }
+};
